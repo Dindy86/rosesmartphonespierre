52228ac25a7875d1198811114335d26262369324
Dindy Pierre <ing.pierredindy@gmail.com>
2022-04-14 23:56:29 -0300

    He modificado app.js y app.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          RoseSmartphones
+        </h1>
       </header>
+      <main>
+        <h2>Productos</h2>
+        <ul>
+          <li>Iphone 11</li>
+          <li>Samsung Galaxy 8 edge</li>
+          <li>Iphone 13</li>
+          <li>Samsung Galaxy 7 edge</li>
+        </ul>
+      </main>
     </div>
   );
 }
